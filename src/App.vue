dcd9ee86c9ebdb9ef879a7a6538acfa5472fce20
nragan34 <ragan.a.nick@gmail.com>
2021-04-25 14:45:54 -0500

    ...

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
+
+  <!-- This is where routes are rendered -->
   <router-view/>
 </template>
 
